2c2d87073d03b61079d15fef0bd04912b464236d
Paulo Radis <paulo_saldanha90@hotmail.com>
2021-11-03 19:13:22 -0300

    Outras diferenças entre var e let

---
+++ aula031/index2.js
@@ -0,0 +1,6 @@
+console.log(sobrenome); 
+let sobrenome = 'Miranda';
+// usar o varariável antes de declarar ela da UNDEFINED...
+// usar o varariável antes de declarar, vai gerar um erro ReferenceError: Cannot access 'sobrenome' before initialization at Object.
+
+// **************
