06171bbde0bf4eb549c112dbfa6a1677e4c9462e
Paulo Radis <paulo_saldanha90@hotmail.com>
2021-11-20 11:36:14 -0300

    Exercício concluido

---
+++ aula041/index.js
@@ -0,0 +1,22 @@
+// Escreva uma função que recebe 2 números e retorne o maior deles.
+
+const numeros2 = (a, b) => a > b ? a : b;
+
+// function numeros(a, b) {
+//   return a > b ? a : b;
+// }
+
+// function numeros(a, b) {
+//   if (a > b) return a;
+//   return b;
+//  }
+
+// function numeros(a, b) {
+//   if (a > b) {
+//     return a;
+//   } else {
+//     return b;
+//   }
+// }
+
+console.log(numeros2(2, 4));
